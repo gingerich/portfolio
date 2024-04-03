a86581dde68d57244288bf234a6aa36bcc64816a
Marlin Gingerich <marlin@gingerich.io>
2024-04-03 17:06:21 -0400

    🚀 connect keystatic cloud

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,8 +2,10 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
-    // repo: `gingerich/portfolio`
+    kind: "cloud"
+  },
+  cloud: {
+    project: 'gingerich/portfolio',
   },
   collections: {
     posts: collection({
